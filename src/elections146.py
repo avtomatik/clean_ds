3e7960672a71ba71d17d798e6b544f77c79f2611
Alexander Mikhailov <extroper@yandex.ru>
2025-05-01 22:53:42 +0300

    Updated `get_election_results()`

--- src/elections146.py
+++ src/elections146.py
@@ -5,26 +5,16 @@ Created on Wed Sep 22 22:23:22 2021
 @author: Alexander Mikhailov
 """
 
-from pandas import DataFrame
+import pandas as pd
+
+from config import ELECTION_RESULTS
 
 
-def main():
-    data = {
-        'Единая Россия': 58.99,
-        'КПРФ': 32.96,
-        'ЛДПР': 23.74,
-        'Справедливая Россия': 19.41,
-        'Яблоко': 9.32,
-        'Партиоты России': 1.46,
-        'Правое дело': 0.59
-    }
-    df = DataFrame(
-        zip(data.keys(), data.values()),
-        columns=['parties', 'percentages']
-    )
-    df['corrected'] = df.iloc[:, 1].div(df.iloc[:, 1].sum())
+def get_election_results(data: dict[str, float]) -> pd.DataFrame:
+    df = pd.DataFrame(list(data.items()), columns=['party', 'percentage'])
+    df['corrected'] = df['percentage'].div(df['percentage'].sum())
     return df
 
 
 if __name__ == '__main__':
-    main()
+    print(get_election_results(ELECTION_RESULTS))
